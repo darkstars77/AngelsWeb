1f2aed7d8be5ad0ec1ec26f6fdcc8b5336aaf9eb
hyeonggon <darkstars77@naver.com>
2016-08-15 17:41:09 +0900

    second commit

--- AngelsWeb/settings.py
+++ AngelsWeb/settings.py
@@ -26,9 +26,9 @@ STATICFILES_DIRS
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
