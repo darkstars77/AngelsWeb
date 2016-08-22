baf4887c0251577097923419475adc052c92213b
hyeonggon <darkstars77@naver.com>
2016-08-22 23:26:46 +0900

    button action add

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main_page, name='main_page'),
+    url(r'^show_write_form/$', views.show_write_form),
+    url(r'^DoWriteBoard/$', views.DoWriteBoard),
+    url(r'^listSpecificPageWork/$', views.listSpecificPageWork),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response , HttpResponseRedirect
 from .models import Post
 from django.utils import timezone
 
@@ -6,3 +6,22 @@ from django.utils import timezone
 def main_page(request):
     #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'WebView/index.html', {})
+
+def show_write_form(request):
+    return render_to_response('WebView/writeBoard.html')
+
+def DoWriteBoard(request):
+    post = Post(title=request.POST['subject'],
+              author=request.POST['name'],
+              text=request.POST['memo'],
+              created_date=timezone.now(),
+              hits=0
+              )
+    post.save()
+
+    # 저장을 했으니, 다시 조회해서 보여준다.
+    url = '/listSpecificPageWork?current_page=1'
+    return HttpResponseRedirect(url)
+
+def listSpecificPageWork(request):
+    return render_to_response('listSpecificPage.html')
