8750f81df57ccaa4c1ed45ed51365e030d80e765
hyeonggon <darkstars77@naver.com>
2016-08-30 21:58:37 +0900

    add db column

--- blog/admin.py
+++ blog/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Post, Product
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['product_number', 'title', 'description', 'image_file', 'Product_satisfaction_by11st', 'pricePos',
-                    'priceNeg', 'deliveryPos', 'deliveryNeg', 'sizePos', 'sizeNeg', 'extraPos', 'extraNeg',
+    list_display = ['product_number', 'title', 'description', 'review', 'image_file', 'Product_satisfaction_by11st',
+                    'pricePos', 'priceNeg', 'deliveryPos', 'deliveryNeg', 'sizePos', 'sizeNeg', 'extraPos', 'extraNeg',
                     'created_at']
 
 admin.site.register(Post)
