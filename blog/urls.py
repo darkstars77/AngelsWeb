919f0562b3fab43f67ec3fbb0b4465c3a4aef91d
hyeonggon <darkstars77@naver.com>
2016-09-01 23:51:31 +0900

    change urls

--- blog/urls.py
+++ blog/urls.py
@@ -5,10 +5,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main_page, name='main_page'),
-    url(r'^show_write_form/$', views.show_write_form),
     url(r'^product/(?P<product_id>\d+)$', views.single_product, name='view_single_product'),
     url(r'^search_product/$', views.search_product),
-    url(r'^search_result/$', views.search_result),
 ]
 if settings.DEBUG:
     urlpatterns += static(
